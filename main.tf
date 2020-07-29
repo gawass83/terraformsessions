6539d51e20bdebbb55b5a95dbd1b8a25a83ce76d
Shubham Gawas <shubhgaw@amdocs.com>
2020-07-29 11:45:21 +0530

    new files added

---
+++ main.tf
@@ -0,0 +1,20 @@
+resource "azurerm_resource_group" "rg" {
+    name = var.resource_group
+    location = var.location["location1"]
+}
+resource "azurerm_virtual_network" "vnet"{
+    name = "TF-virtualnetwork1"
+    location = "${azure_resource_group.rg.location}"
+    resource_group_name = "${azure_resource_group.rg.name}"
+    address_space = ["10.0.0.0/16"]
+    dns_servers = ["10.0.0.4","10.0.0.5"]
+
+    subnet {
+        name = "subnet1"
+        address_prefix = var.subnets[0]
+    }
+    subnet {
+        name = "subnet2"
+        address_prefix = var.subnets[1]
+    }
+}
